a28279ffdd6c64a6c63170ec798c6378a3ccd982
Pranav30072004 <pranavrpathak04@gmail.com>
2026-02-25 20:42:44 +0530

    added ability to call and display color scheme from thecolorapi website

--- Color Generator.js
+++ Color Generator.js
@@ -1,22 +1,30 @@
 const colorChoiceInput = document.getElementById('color-picker');
 const schemeChoiceInput = document.getElementById('scheme-selector');
 const colorSchemeBtn = document.getElementById('scheme-generator-btn');
+const schemeContainer = document.getElementById('color-scheme-container');
 
 
 function generateScheme(color, mode) {
-   fetch(`https://thecolorapi.com/scheme?hex=${color}&mode=${mode}`, {method: "GET"})
+   fetch(`  https://www.thecolorapi.com/scheme?hex=${color}&mode=${mode}`)
    .then(res => res.json())
    .then(data => {
-       console.log(data);
+       let schemeArray = data.colors;
+       let colorSchemeHtml = ``
+       for(scheme of schemeArray) {
+           colorSchemeHtml += `
+           <img src="${scheme.image.bare}" id="color-img" />
+           <p class="color-hex">${scheme.hex.value}</p>
+           `
+       }
+       console.log(colorSchemeHtml);
+       schemeContainer.innerHTML = colorSchemeHtml;
    });
 
 }
 
 colorSchemeBtn.addEventListener('click', () => {
-    let colorChoice = colorChoiceInput.value;
+    let colorChoice = colorChoiceInput.value.replace("#", "");
     let schemeChoice = schemeChoiceInput.value;
 
-    generateScheme(colorChoice, schemeChoice)
-
-
+    generateScheme(colorChoice, schemeChoice);
 })
